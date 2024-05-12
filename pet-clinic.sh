d9a43bc156fdf647e628f05f564b8d58aae1dfc3
Mahmoud Sabra <mahmoudsabraa25@gmail.com>
2024-05-12 17:16:10 +0300

    Update pet-clinic.sh

--- pet-clinic.sh
+++ pet-clinic.sh
@@ -6,21 +6,24 @@ if id "pet-clinic" &>/dev/null; then
 else
     # If not, create a new functional user called pet-clinic
     sudo useradd -m pet-clinic
+    passwd pet-clinic   # Set a password for the user
     echo "User pet-clinic created successfully."
-    sudo passwd pet-clinic
 fi
 
 # Check if Java is installed
-if ! /home/pet-clinic/jdk-22.0.1/bin/java --version &> /dev/null; then
+if ! /home/pet-clinic/jdk-17.0.11/bin/java --version &> /dev/null; then
     echo "Java is not installed. Downloading and installing..."
 
     # Check if the JDK package exists in the current directory
-    cd /home/pet-clinic
-    wget https://download.oracle.com/java/22/latest/jdk-22_linux-x64_bin.tar.gz
-    tar -xf jdk-22_linux-x64_bin.tar.gz
+    if [ ! -f "/home/pet-clinic/jdk-17_linux-x64_bin.tar.gz" ]; then
+        # If the package doesn't exist, download it
+        cd /home/pet-clinic
+        wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz
+        tar -xf jdk-17_linux-x64_bin.tar.gz
+    fi
 
     # Add Java bin directory to pet-clinic user's PATH
-    echo 'export PATH="$PATH:/home/pet-clinic/jdk-22.0.1/bin"' >> /home/pet-clinic/.bashrc
+    echo 'export PATH="$PATH:/home/pet-clinic/jdk-17.0.11/bin"' >> /home/pet-clinic/.bashrc
 else
     echo "Java is already installed."
 fi
